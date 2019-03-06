750703c400e8f7d9c6b6b8f738e49af13109686a
Daniel Esteban <dolardanies@hotmail.com>
2019-03-06 16:50:05 -0500

    Inicio

---
+++ src/test/java/com/edu/eci/ieti/task_planner_api/TaskPlannerApiApplicationTests.java
@@ -0,0 +1,16 @@
+package com.edu.eci.ieti.task_planner_api;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class TaskPlannerApiApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
